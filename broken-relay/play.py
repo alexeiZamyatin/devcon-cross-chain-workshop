e74ed7cf35296fa19232776d99f6268e3d2e548b
Dominik Harz <dominik.harz@gmail.com>
2019-10-10 00:45:30 +0900

    add play script

--- broken-relay/play.py
+++ broken-relay/play.py
@@ -13,21 +13,23 @@ import requests
 BASE = "http://localhost:3000"
 TESTS = {
     "1": False,
-    "2": False, 
-    "3a": False, 
+    "2": False,
+    "3a": False,
     "3b": False,
     "4": False,
     "5": False,
     "6": False,
     "7": False,
-    "8": False, 
+    "8": False,
     "9": False
 }
+
 
 def print_file(name):
     with open(os.path.join("docs", name), "r") as file:
         text = file.read()
     print(text)
+
 
 def read_config():
     try:
@@ -35,36 +37,43 @@ def read_config():
             config = json.load(file)
     except FileNotFoundError:
         config = {}
-    
+
     return config
+
 
 def update_config(config):
     with open("config.json", "w+") as file:
         json.dump(config, file)
 
+
 def execute(cmd):
-    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    popen = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, universal_newlines=True)
     for stdout_line in iter(popen.stdout.readline, ""):
-        yield stdout_line 
+        yield stdout_line
     popen.stdout.close()
     return_code = popen.wait()
     if return_code:
         raise subprocess.CalledProcessError(return_code, cmd)
 
+
 def init():
+    os.system('cls' if os.name=='nt' else 'clear')
     print_file("banner.txt")
     print_file("intro.txt")
 
+
 def user_input(message):
     if sys.version_info[0] < 3:
-        my_input = raw_input(message) 
+        my_input = raw_input(message)
     else:
         my_input = input(message)
 
     return my_input
 
+
 def register():
-    URL = BASE + "/api/register"
+    url = BASE + "/api/register"
     name = None
 
     while not name:
@@ -72,9 +81,10 @@ def register():
     data = json.dumps({"name": name})
 
     try:
-        request = requests.post(URL, headers={'Content-Type': 'application/json' }, data=data)
+        request = requests.post(
+            url, headers={'Content-Type': 'application/json'}, data=data)
         response = request.json()
-    
+
     except:
         print("Something went wrong with the server")
 
@@ -85,13 +95,15 @@ def register():
 
     print(response["message"])
 
+
 def hint():
     # return which cases are not yet solved
     print("")
     # ask which case is solved now
-    next_test = user_input("Please enter the number of the attack you want to have a hint for: ")
+    next_test = user_input(
+        "Please enter the number of the attack you want to have a hint for: ")
 
-    URL = BASE + "/api/hint?id={}&case={}".format(config["id"], next_test)
+    url = BASE + "/api/hint?id={}&case={}".format(config["id"], next_test)
 
     open_problems = []
     for key, value in config["tests"].items():
@@ -106,11 +118,11 @@ def hint():
 
     # Print which test case is submitted
     # print("You are submitting the solution for case {}".format(next_test))
-    
+
     # get the testcase from the server
     try:
         # submit team_id
-        request = requests.get(URL)
+        request = requests.get(url)
         # print(request.status_code)
         # print(request._content)
         response = request.json()
@@ -123,10 +135,10 @@ def hint():
             test_file.write(response["content"])
     except:
         print("Hint already fetched from the server")
- 
+
 
 def submit():
-    URL = BASE + "/api/submit"
+    url = BASE + "/api/submit"
 
     print("Upgrading defenses...")
     # run tests locally
@@ -142,7 +154,7 @@ def submit():
                 # list[1] is test case number plus any additional information
                 output_list = output.split(" TESTCASE ", 1)
                 # list[1] looks like "1: set ....". Split at : and return the first elemet
-                testcase = output_list[1].split(":",1)[0]
+                testcase = output_list[1].split(":", 1)[0]
                 # store the result of the testcase
                 if "✓" in output_list[0]:
                     results[testcase] = True
@@ -151,11 +163,12 @@ def submit():
                     print("You successfully completed testcase {}.".format(testcase))
                 else:
                     results[testcase] = False
-                    print("Sorry, testcase {} failed. TIP: you can request a hint and see the testcase in the 'test' folder.".format(testcase))
+                    print("Sorry, testcase {} failed. TIP: you can request a hint and see the testcase in the 'test' folder.".format(
+                        testcase))
         print("===== Congratulations! You have completed the game!")
     except CalledProcessError:
         print("===== Oh no, you are still vulnerable! ====")
-    
+
     # report results to server
     # prepare submission with team id
     data = json.dumps({
@@ -165,7 +178,8 @@ def submit():
 
     # submit
     try:
-        request = requests.post(URL, headers={'Content-Type': 'application/json' }, data=data)
+        request = requests.post(
+            url, headers={'Content-Type': 'application/json'}, data=data)
         response = request.json()
 
         print(response["message"])
@@ -176,9 +190,9 @@ def submit():
 
 
 def leaders():
-    URL = BASE + "/api/leaderboard"
+    url = BASE + "/api/leaderboard"
     # get the leaderboard
-    request = requests.get(URL)
+    request = requests.get(url)
     # returns a sorted list of teams
     response = request.json()
 
@@ -196,10 +210,11 @@ def leaders():
 
     # print(json.dumps(response))
 
+
 def score():
-    URL = BASE + "/api/score?id={}".format(config["id"])
+    url = BASE + "/api/score?id={}".format(config["id"])
     # get your current score and rank
-    request = requests.get(URL)
+    request = requests.get(url)
     response = request.json()
     team = response["team"]
 
@@ -214,6 +229,7 @@ def score():
         else:
             print("Testcase {:2}: {:2}".format(key, score))
 
+
 def test():
     try:
         for output in execute(["truffle", "test"]):
@@ -221,16 +237,18 @@ def test():
     except CalledProcessError:
         print("===== Tests failed ====")
 
+
 def display_help():
     print("You have the following options:\n"
-        "hint:    Asks you to submit a testcase number and gives you the testcase.\n"
-        "         Requesting a hint will reduce the score you can get for that testcase by 50%.\n"
-        "test:    Locally executes 'truffle test' to give you feedback on your contract.\n"
-        "submit:  Evaluates your contract and you will get a score.\n"
-        "score:   See your current score for each testcase.\n"
-        "leaders: Displays the leaderboard.\n"
-        "help:    Displays this help.\n"
-        "quit:    End the game.\n")
+          "hint:    Asks you to submit a testcase number and gives you the testcase.\n"
+          "         Requesting a hint will reduce the score you can get for that testcase by 50%.\n"
+          "test:    Locally executes 'truffle test' to give you feedback on your contract.\n"
+          "submit:  Evaluates your contract and you will get a score.\n"
+          "score:   See your current score for each testcase.\n"
+          "leaders: Displays the leaderboard.\n"
+          "help:    Displays this help.\n"
+          "quit:    End the game.")
+
 
 def stop():
     print("Thanks for playing!")
@@ -244,8 +262,10 @@ if __name__ == "__main__":
     register()
     command = None
     while not command:
-        command = user_input("What would you like to do next?\n(hint/test/submit/score/leaders/help/quit): ")
-
+        print("")
+        command = user_input(
+            "What would you like to do next?\n(hint/test/submit/score/leaders/help/quit): ")
+        print("")
         if command == "help":
             display_help()
         elif command == "hint":
@@ -261,6 +281,5 @@ if __name__ == "__main__":
         elif command == "leaders":
             leaders()
         else:
-            print(command)
-            print("Command not understood. Type 'help' for help and 'quit' to exit.")
-        command= None
+            print("'{}' not understood. Type 'help' for help and 'quit' to exit.".format(command))
+        command = None
